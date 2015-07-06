01e9660758193a439467d15126d5a48f6caf2776
Michael Corkum <mcorkum@gmail.com>
2015-07-06 14:30:06 -0400

    FighterJet.IO Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# fighterjet.io
+# FighterJet.IO
 The fastest PHP framework in the world.
+
+FighterJet.IO is Mobile, Social and Native.
---
+++ src/fighterjet.php
@@ -0,0 +1,18 @@
+<?php
+
+	namespace Maverick;
+
+	class FighterJet
+	{
+
+		public function __construct()
+		{
+			$this->exit_like_a_boss();
+		}
+
+		private function exit_like_a_boss()
+		{
+			exit();
+		}
+
+	}
