9f4cb2e537cc44da97e45f417e0b52747d9381cb
Michael Corkum <mcorkum@gmail.com>
2015-07-06 14:30:25 -0400

    Made fighterjet.io more awesome

--- src/fighterjet.php
+++ src/fighterjet.php
@@ -2,14 +2,33 @@
 
 	namespace Maverick;
 
+	/**
+	 * FighterJet
+	 *
+	 * This mega powerhouse of a class allows for you to exit with authority.
+	 *
+	 * @author Mike Corkum <mcorkum@gmail.com>
+	 * @copyright 2015 Kick Paul Blanco in the Face Studios
+	 * @license http://www.php.net/license/3_01.txt PHP License 3.01
+	 */
+
 	class FighterJet
 	{
-
+		/**
+		 * Exits like a fucking champion
+		 *
+		 * @param parameters will only slow FighterJet.io down
+		 */
 		public function __construct()
 		{
 			$this->exit_like_a_boss();
 		}
 
+		/**
+		 * FighterJet.IO Core Function
+		 *
+		 * @param awesomeness cannot be defined.
+		 */
 		private function exit_like_a_boss()
 		{
 			exit();
